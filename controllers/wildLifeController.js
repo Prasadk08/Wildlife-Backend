f5cc02a2807ad25c40d3f48398c5fff01b682855
Prasadk08 <prasadkshirsagar1199@gmail.com>
2025-08-07 12:56:36 +0530

    new backend changes

--- controllers/wildLifeController.js
+++ controllers/wildLifeController.js
@@ -1,10 +1,11 @@
-import WildLife from '../model/wildLife.js'
+
+import AnimalCard from '../model/animalcard.js';
 
 
 export const getAllWildlife = async (req, res) => {
   try {
     console.log("Fetching all wildlife data");
-    const wildlife = await WildLife.find();
+    const wildlife = await AnimalCard.find();
     res.status(200).json(wildlife);
   } catch (error) {
     console.error("Error fetching wildlife:", error);
